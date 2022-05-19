b08bd61bea3cfd0e057d1427bfb1603b5124ba31
Brian B <brtl567@gmail.com>
2022-05-19 14:02:45 -0400

    first version Done

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const bodyPasrser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app =express();
 
-var items = [];
-let workItems = [];
+const items = [];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -13,22 +14,14 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
     
-    var today = new Date();
-
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    var day = today.toLocaleDateString("en-US", options);
+ const day = date.getDate();   
     
     res.render('list', {listTitle: day, newListItems: items});
 });
 
 app.post("/", function(req, res) {
     
-    let item = req.body.newItem;
+    const item = req.body.newItem;
 
     if (req.body.list === "Work") {
         workItems.push(item);
@@ -48,7 +41,7 @@ app.get("/about", function(req, res) {
 });
 
 app.post("/work", function(req, res) {
-    let item = req.body.newItem;
+    const item = req.body.newItem;
     workItems.push(item);
     res.redirect("/work");
 });
